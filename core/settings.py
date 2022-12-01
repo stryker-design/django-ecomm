00470a245d1d7388d3144b5c866f739b71d020f2
Stryker-Design <andy@stryker-design.io>
2022-12-01 13:24:18 -0500

    Allowed hosts update

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = '3xk*)i0x#k$btl=(6q)te!19=mp6d)lm1+zl#ts4ewxi3-!vm_'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['yourdomain.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
