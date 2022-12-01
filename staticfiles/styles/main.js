e2c6b3814d3a276e0c10bed900208c55cc200c9c
Stryker-Design <andy@stryker-design.io>
2022-12-01 12:59:21 -0500

    Adding collect static

---
+++ staticfiles/styles/main.js
@@ -0,0 +1,26 @@
+// MY ACCOUNT DROPDOWN
+
+const myAccount = document.getElementById("my-account");
+const dropDown = document.getElementById("dropdown");
+
+// myAccount.addEventListener("click", function () {
+//   dropDown.classList.toggle("hidden");
+// });
+
+// CATEGORY DROPDOWN
+
+const category = document.getElementById("category");
+const categoryDropDown = document.getElementById("category-dropdown");
+
+category.addEventListener("click", function () {
+  categoryDropDown.classList.toggle("hidden");
+});
+
+// BASKET DROPDOWN
+
+// const shoppingBasket = document.getElementById("basket");
+// const basketDropdown = document.getElementById("basket-dropdown");
+
+// shoppingBasket.addEventListener("click", function () {
+//   basketDropdown.classList.toggle("hidden");
+// });
